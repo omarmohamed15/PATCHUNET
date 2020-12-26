da5fccf205466e9be14505f838ba0a2f6b2b4705
Omar M. Saad <engomar91@gmail.com>
2020-12-26 16:39:42 +0200

    Update Step1_Patching.m

--- Step1_Patching.m
+++ Step1_Patching.m
@@ -1,11 +1,21 @@
+Omar M. Saad
 % Preparing the patches for the PATCHUNET
 clc
 clear 
 close all
 
 % Loading the Data 
-% Synthetic Example
-load synthetic_example
+% Synthetic or Field Example, zero for synthetic and one for field example.
+select =1;
+
+switch select
+    case 0
+    load synthetic_example
+    % obtain the SNR of the noisy data (only valid for synthetic example)
+    fprintf('The SNR of the Noisy data is %0.3f \n',yc_snr(DataClean,DataNoisy))
+    case 1
+    load field_example
+end
 
 % preparing the patches where w1 and w2 are the patch size, while the s1z
 % and s2z are the number of shift samples between neighbor windows. The
@@ -19,5 +29,4 @@ dn_patch = yc_patch(dn1,1,w1,w2,s1z,s2z);
 % It is better to save .mat as -V 7.3 or later because the size of the generated patch is large.
 save Input_Patches dn_patch
 
-% obtain the SNR of the noisy data 
-fprintf('The SNR of the Noisy data is %0.3f \n',yc_snr(DataClean,DataNoisy))
+
